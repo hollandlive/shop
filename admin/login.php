7a938e56dde1c466516f216ea59b7dca5e8e8ad8
Artem <dutchnewsru@gmail.com>
2015-10-06 15:45:13 +0200

    login function update
    i use PDO connection for connecting to MySQL database

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,20 @@
 <?php
 function login()
 {
-	$con = mysqli_connect("localhost", "test", "test", "store") or die('couldnt connect to server');
-	return $con;
+	$servername = "localhost";
+	$username = "xxxx";
+	$password = "xxxx";
+
+	try {
+	    $conn = new PDO("mysql:host=$servername;dbname=store", $username, $password);
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    echo "Connected successfully"; 
+	    }
+	catch(PDOException $e)
+	    {
+	    echo "Connection failed: " . $e->getMessage();
+	    }
+	return $conn;
 }
 ?>
